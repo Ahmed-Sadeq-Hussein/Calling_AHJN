3c417ffe13d2fe2887dd34cfa0c1dad401cf7121
Ash <huah21vn@student.ju.se>
2026-04-28 12:00:43 +0200

    push into repo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Calling_AHJN
 A Deep learning project focused on using a from scratch language to implement a working TTS VC applications, Called AHJN
+
+Authors : Ahmed Sadeq Hussein and Julius Norén 
+
+
+
+Step 1 . Download LJSpeech and train a model from scratch on it . Data will be stored within the path of a config file that is meant to be edited by the user
---
+++ config.py
@@ -0,0 +1,9 @@
+# Here you insert your Path to the folder of the training voice 
+training_voice = "C:" 
+# Here you insert your Path to the folder of the target voices. We have multiple of these in order to do this experiment multiple times 
+target_voices = ["C:"]
+# Here we write the name of the folder where the run experiment is meant to be made inside .
+experiment_name = "Experiment_1" 
+# Here we insert metadata into the recording in order to identify the voice to make sure no mix up happens for training purposes 
+training_voice_metadata = "Training_Voice" + experiment_name
+# Here we insert metadata into the recording in order to identify the voice to make sure no mix
