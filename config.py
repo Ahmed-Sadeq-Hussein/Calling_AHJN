0b06d68d3ca0e83fd2841a8340cb18a3e3d404bb
Baesar <noju22ez@student.ju.se>
2026-05-08 16:07:34 +0200

    commiting yeah

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Here you insert your Path to the folder of the training voice 
-training_voice = "C:" 
+training_voice = "" 
 # Here you insert your Path to the folder of the target voices. We have multiple of these in order to do this experiment multiple times 
-target_voices = ["C:"]
+target_voices = []
 # Here we write the name of the folder where the run experiment is meant to be made inside .
 experiment_name = "Experiment_1" 
 # Here we insert metadata into the recording in order to identify the voice to make sure no mix up happens for training purposes 
